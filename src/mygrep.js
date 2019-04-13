21015719e5a21cc3cb51fe51b847832ab43b29bb
Woojae Park <wjp241@nyu.edu>
2019-04-13 06:44:35 -0400

    deletd unnecessary files and simulated dir walk via recursion/ next step implement cli

--- src/mygrep.js
+++ src/mygrep.js
@@ -1,15 +1,16 @@
 var fs = require('fs');
 var path = require('path');
-const subdirs = fs.readdirSync('stories');
-function a(dir = 'stories') {
+function a(dir = path.join(__dirname, "stories")) {
+  console.log(dir)
+  const subdirs = fs.readdirSync(dir);
   let files = [], dirs = [];
   for (let i = 0; i < subdirs.length; i++) {
-    if (fs.statSync('stories/' + subdirs[i]).isDirectory()) dirs.push(subdirs[i]);
-    if (fs.statSync('stories/' + subdirs[i]).isFile()) files.push(subdirs[i]);
+    if (fs.statSync(`${dir}/` + subdirs[i]).isDirectory()) dirs.push(subdirs[i]);
+    if (fs.statSync(`${dir}/` + subdirs[i]).isFile()) files.push(subdirs[i]);
   }
 
   for (let j = 0; j < files.length; j++) {
-    const inputStr = fs.readFileSync('stories/' + files[j], { encoding: "utf-8" }).trim();
+    const inputStr = fs.readFileSync(`${dir}/` + files[j], { encoding: "utf-8" }).trim();
     const inputArr = inputStr.split('\n');
     for (let k = 0; k < inputArr.length; k++) {
       if (inputArr[k].includes('like')) console.log(`${path.join(__dirname, files[j])}:${k}\t ${inputArr[k]}`);
@@ -17,7 +18,8 @@ function a(dir = 'stories') {
   }
 
   for (let l = 0; l < dirs.length; l++) {
-
+    a(path.join(dir, dirs[l]))
   }
 }
 
+a()
